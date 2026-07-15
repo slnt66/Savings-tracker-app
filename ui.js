58ecc880c8564d53ecf234b7a3d646ef03b174ef
slnt66 <slntreply@gmail.com>
2026-07-15 09:25:51 +0300

    feat&fix: Added adaptive styles, fixed search bar

--- ui.js
+++ ui.js
@@ -1,48 +1,54 @@
 // ------- search functions
 const searchContainer  = document.querySelector(".search"); 
+const searchInput = document.getElementById('searchInput')
 
 function expandSearch(){
-    if (!searchContainer.classList.contains('expanded'))
+    if (!searchContainer.classList.contains('expanded')){
         searchContainer.classList.add('expanded');
+    }
 }
 
 function collapseSearch() {
-    if (searchContainer.classList.contains('expanded')) 
+    if (searchContainer.classList.contains('expanded')) {
         searchContainer.classList.remove('expanded');
+       
+    }
+        
 }
 document.addEventListener('click', function(e) {
     if (!searchContainer.contains(e.target)) 
         collapseSearch();
+        
 });
 
 document.addEventListener('keydown', function(e) {
     if (e.key === 'Escape' && searchContainer.classList.contains('expanded')) {
         collapseSearch();
-        searchInput.blur(); 
+        // searchInput.blur(); 
     }
 });
 
-searchContainer.addEventListener('click', function(e) {
-    if(!searchContainer.classList.contains('expanded')){
-        e.stopPropagation();  // Чтобы не сработал клик на контейнере
+function pressedEffect(element, callback){
+    if(!element.classList.contains('expanded')){
+        element.classList.add('pressed');
         
-        // Добавляем класс для анимации нажатия
-        searchContainer.classList.add('pressed');
-        
-        // Убираем класс после окончания анимации
         setTimeout(() => {
-            searchContainer.classList.remove('pressed');
-            expandSearch();
-        }, 100);  // 500ms = длительность анимации
+            element.classList.remove('pressed');
+            if (typeof callback === 'function') 
+                callback();
+        }, 150); 
     }
-});
+}
 
+searchContainer.addEventListener('click',() => pressedEffect(searchContainer, expandSearch));
+const sortByBtn = document.getElementById('sortByBtn');
+sortByBtn.addEventListener('click', () => pressedEffect(sortByBtn));
 //------- new goal button 
 
 const newGoalBtn = document.getElementById("newGoalBtn");
 const newGoalModal = document.getElementById("newGoalModal");
 const closeNewGoalModal = document.getElementById("closeNewGoalModal");
-
+const modalContent = document.querySelector(".modal_content")
 
 function displayNone(element){
     element.classList.remove('active');
@@ -50,16 +56,23 @@ function displayNone(element){
 function displayActive(element){
     element.classList.add('active');
 }
-
-newGoalBtn.addEventListener('click', () => displayActive(newGoalModal));
+document.addEventListener('click', function(e) {
+    if (!modalContent.contains(e.target)) 
+        displayNone(newGoalModal);
+        
+});
+newGoalBtn.addEventListener('click', () => {
+    pressedEffect(newGoalBtn, () => displayActive(newGoalModal));
+});
 closeNewGoalModal.addEventListener('click', () => displayNone(newGoalModal));
 
+// -------total savings hover effect 
 
 // Hover total savings card effect 
 // include lib/vanilla-tilt.min.js 
-const card = document.getElementById('total_savings');
+const statsCard = document.querySelector('.user_statistics > .stat_card');
 
-VanillaTilt.init(card, {
+VanillaTilt.init(statsCard, {
     max:3,
     glare: true,
     'max-glare': 0.2,
